1dfd15ebf38b8fde23bd798fdeb99d3be0567547
Princend <waxdji3g4y3c04@gmail.com>
2020-11-15 12:15:47 +0800

    add selector

--- src/app/store/selectors/user.selectors.ts
+++ src/app/store/selectors/user.selectors.ts
@@ -1,2 +1,13 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState } from '..';
+import * as fromReducer from '../reducers/user.reducer'
 
+export const getIsLogin = (state: fromReducer.UsersState) => state.isLogin;
+export const getCurrentUser = (state: fromReducer.UsersState) => state.currentUser;
+
+
+export const getUserState = (state: AppState) => state.user;
+
+export const selectIsLogin = createSelector(getUserState, getIsLogin);
+
+export const selectCurrentUser = createSelector(getUserState, getCurrentUser);
